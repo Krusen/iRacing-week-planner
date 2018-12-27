aa9dbe34d2c154b7dcfd80cb54022c10e9d99bfd
Søren Kruse <krusenen@gmail.com>
2018-12-27 11:29:38 +0100

    Run phantomjs as part of scrapeData script

--- scrape.js
+++ scrape.js
@@ -1,3 +1,4 @@
+import { run as phantomjs } from 'phantomjs-prebuilt';
 import { remote } from 'webdriverio';
 import fs from 'fs';
 import path from 'path';
@@ -5,13 +6,8 @@ import { promisify } from 'bluebird';
 
 const writeFile = promisify(fs.writeFile);
 
-const client = remote({
-  logLevel: 'command',
-  desiredCapabilities: { browserName: 'phantomjs' }
-}).init();
-
-const username = process.env.IWP_USERNAME || 'test';
-const password = process.env.IWP_PASSWORD || 'test';
+const username = process.env.IWP_USERNAME;
+const password = process.env.IWP_PASSWORD;
 
 const extractJSONString = (sourceLines, variableName, fileName) => {
   const regexp = new RegExp(`^var ${variableName} = extractJSON\\('`);
@@ -28,18 +24,26 @@ const extractJSONString = (sourceLines, variableName, fileName) => {
   writeFile(path.join(__dirname, fileName), JSON.stringify(listing, null, 2));
 };
 
-client
-  .url('https://members.iracing.com/membersite/login.jsp')
-  .addValue('[name="username"]', username)
-  .addValue('[name="password"]', password)
-  .click('input.log-in')
-  .url('http://members.iracing.com/membersite/member/Series.do')
-  .source()
-  .then(source => {
-    const sourceLines = source.value.split('\n');
-    extractJSONString(sourceLines, 'TrackListing', 'src/data/tracks.json');
-    extractJSONString(sourceLines, 'CarClassListing', 'src/data/car-class.json');
-    extractJSONString(sourceLines, 'CarListing', 'src/data/cars.json');
-    extractJSONString(sourceLines, 'SeasonListing', 'src/data/season.json');
-  })
-  .end();
+phantomjs('--webdriver=4444').then(program => {
+  const client = remote({
+    logLevel: 'command',
+    desiredCapabilities: { browserName: 'phantomjs' }
+  }).init();
+
+  client
+    .url('https://members.iracing.com/membersite/login.jsp')
+    .addValue('[name="username"]', username)
+    .addValue('[name="password"]', password)
+    .click('input.log-in')
+    .url('http://members.iracing.com/membersite/member/Series.do')
+    .source()
+    .then(source => {
+      const sourceLines = source.value.split('\n');
+      extractJSONString(sourceLines, 'TrackListing', 'src/data/tracks.json');
+      extractJSONString(sourceLines, 'CarClassListing', 'src/data/car-class.json');
+      extractJSONString(sourceLines, 'CarListing', 'src/data/cars.json');
+      extractJSONString(sourceLines, 'SeasonListing', 'src/data/season.json');
+      program.kill();
+    })
+    .end();
+})
